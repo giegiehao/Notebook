d3178a512884a996159d8dda2cb757272db4a017
giegiehao <2460233267@qq.com>
2023-04-19 20:15:39 +0800

    main方法反射

---
+++ javafild/Maintext.java
@@ -0,0 +1,18 @@
+package javafild;
+
+import java.lang.reflect.Method;
+
+public class Maintext{
+      public static void main(String[] args) throws Exception{
+        Class clazz = Class.forName("javafild.Main");
+
+        Method method = clazz.getMethod("main", String[].class);
+        method.invoke(null, (Object)new String[]{});
+    }
+}
+
+class Main{
+  public static void main(String[] args) {
+    System.out.println("this is Main.main");
+  }
+}
