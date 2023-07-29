27eb425b65aef120f17f72a23eb9aa0b7635afac
giegiehao <2460233267@qq.com>
2023-07-29 23:16:17 +0800

    账单计算器

---
+++ src/Main.java
@@ -0,0 +1,78 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    static int i = 0;
+    static int scale = 2;
+    static RoundingMode roundingMode = RoundingMode.HALF_UP;
+
+    public static BigDecimal summation(){
+        BigDecimal sum = new BigDecimal(0);
+        BigDecimal quantity;
+        BigDecimal unit_price;
+        BigDecimal amount;
+        String s = "";
+        int x = 0;
+        Scanner scanner = new Scanner(System.in);
+
+
+        while (true) {
+            System.out.println("* 第" + (++x) + "项数量及单价：");
+            System.out.print("数量：");
+//            try{
+//                quantity = scanner.nextDouble();
+//                System.out.print("单价：");
+//                unit_price = scanner.nextDouble();
+//                amount = quantity * unit_price;
+//                System.out.println("金额：" + amount);
+//                sum += amount;
+//            } catch (InputMismatchException e){
+//                System.out.println("该账单已结束");
+//                break;
+//            }
+
+            try {
+                s = scanner.nextLine();
+                if (s.isEmpty()) s = "1";
+                quantity = new BigDecimal(s);
+                System.out.print("单价：");
+                s = scanner.nextLine();
+                unit_price = new BigDecimal(s);
+                amount = Main.bigSum(quantity, unit_price);
+                System.out.println("金额：" + amount);
+                sum = sum.add(amount).setScale(scale,roundingMode);
+
+            } catch (NumberFormatException e){
+                if (s.endsWith("\\")){
+                    System.out.println("已撤销上一条数据");
+                    x--;
+                }else {
+                    System.out.println("该账单已结束");
+                    break;
+                }
+            }
+        }
+
+
+        return sum;
+    }
+
+    public static BigDecimal bigSum(BigDecimal quantity, BigDecimal unit_price){
+
+        return quantity.multiply(unit_price).setScale(scale,roundingMode);
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("欢迎进入账单计算系统");
+
+        while (true) {
+            System.out.println("第" + (++i) + "张账单");
+            System.out.println("总数:" +  Main.summation());
+            Thread.sleep(50);
+            System.out.println("-------------------------------------------------------------");
+        }
+    }
+}
